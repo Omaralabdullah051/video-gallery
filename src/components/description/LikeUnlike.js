9a03a9e1e509407fae7e9caf6980dd657a5cfb24
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-08-28 17:44:09 +0400

    video feature added

--- src/components/description/LikeUnlike.js
+++ src/components/description/LikeUnlike.js
@@ -7,14 +7,12 @@ const LikeUnlike = () => {
     <div class="flex gap-10 w-48">
       <div class="flex gap-1">
         <div class="shrink-0">
-          {/* <img class="w-5 block" src="./assets/like.svg" alt="Like" /> */}
           <Like />
         </div>
         <div class="text-sm leading-[1.7142857] text-slate-600">100K</div>
       </div>
       <div class="flex gap-1">
         <div class="shrink-0">
-          {/* <img class="w-5 block" src="./assets/unlike.svg" alt="Unlike" /> */}
           <Unlike />
         </div>
         <div class="text-sm leading-[1.7142857] text-slate-600">100K</div>
