8d241c7236ce5947c0ed9b6519b4bddff1189757
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-09-06 17:27:17 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import Home from "./pages/Home";
 import Videos from "./pages/Videos";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 function App() {
+  const Wrapper = ({ children }) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children;
+  };
+
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/videos/:videoId" element={<Videos />} />
-      </Routes>
-      <Footer />
+      <Wrapper>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos/:videoId" element={<Videos />} />
+        </Routes>
+        <Footer />
+      </Wrapper>
     </Router>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,27 @@
 import React from "react";
-import Lws from "../../assets/svgImages/Lws";
 import SearchSvg from "../../assets/svgImages/SearchSvg";
 import Search from "./Search";
 import { Link } from "react-router-dom";
+import logo from "../../assets/gallery.png";
 
 const navbar = () => {
   return (
     <nav className="bg-slate-100 shadow-md">
       <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
-        <Link to="/">
-          <Lws />
-        </Link>
+        <div className="flex justify-center items-center gap-2">
+          <div>
+            <Link to="/">
+              <img className="h-10" src={logo} alt="Learn with Sumit" />
+            </Link>
+          </div>
+          <div>
+            <p className="font-bold cursor-pointer">
+              <Link to="/">
+                <i>Video Gallery</i>
+              </Link>
+            </p>
+          </div>
+        </div>
         <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
           <Search />
           <SearchSvg />
